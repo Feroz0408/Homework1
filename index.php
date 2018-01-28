7a4c15bb481eb6862d692578198c2f08021d5f2d
Feroz0408 <Feroz0408@users.noreply.github.com>
2018-01-28 18:19:33 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,308 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>First Excercise</title>
+
+      
+    <!-- Bootstrap core CSS -->
+    <link href="Homeworks/Homework1/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+    <link href="Homeworks/Homework1/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="Homeworks/Homework1/vendor/devicons/css/devicons.min.css" rel="stylesheet">
+    <link href="Homeworks/Homework1/vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
+
+
+    <!-- Custom styles -->
+    <link href="Homeworks/Homework1/css/resume.min.css" rel="stylesheet">
+
+	<!-- 		my style sheet -->
+	<link href="Homeworks/Homework1/style.css" rel="stylesheet">
+	
+      	<!-- 		my Script  -->
+
+      	<link href="Homeworks/Homework1/js/myscript1.js" rel="stylesheet">
+
+	
+  </head>
+ 
+
+
+  <body id="page-top">
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <span class="d-block d-lg-none">Web Technologies II</span>
+        <span class="d-none d-lg-block">
+          <img class="img-fluid img-profile rounded-circle mx-auto mb-2 border shrink" src="Homeworks/Homework1/img/img.png"    alt="not found">
+        </span>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">      
+        <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+          </li>
+        <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#Gallery">My Gallery</a>
+          </li>    
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#Homeworks">Homeworks</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#Exercises">In-Class Exercises</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#Projects">Projects</a>
+          </li>          
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container-fluid p-0">
+
+      <section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
+        <div class="my-auto">
+          <h1 class="mb-0">Feroz
+            <span class="text-primary">Shaik</span>
+          </h1>
+            
+          <div class="subheading mb-5"><p>98 Westview Terrace Rochester, NY 14623 - (585) 754-3807 -
+            <a href="mailto:name@email.com">fs2329@g.rit.edu</a></p>
+                      <ul class="list-inline list-social-icons mb-0">
+            <li class="list-inline-item">
+              <a href="https://www.linkedin.com/in/feroz-shaik-25b323108/">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://rit.joinhandshake.com/users/6933469">
+                <span class="fa-stack fa-lg">
+                    <i class="fa fa-handshake-o" style="font-size:53px"></i>
+                  <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            </ul>  
+             <p class="mb-5" id="pp" style="font-family: Fira Sans;font-size: 0.9em;text-align: left;">Hi guys, This is Feroz Shaik and I'm from INDIA. I'm a Graduate Student in IST. I worked as a Database Developer for about 14 months in Tata Consultancy Services. I really enjoy playing Badminton and Cricket in my leisure time.<br/>DOB - 01/09/1994 
+            </p>
+          </div>
+            
+            
+
+
+
+     <div class="resume-item d-flex flex-column flex-md-row mb-5">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Jawaharlal Nehru Technological University</h3>
+              <div class="subheading mb-3">Bachelor of Science</div>
+              <div>Instrumentation</div>
+              <p>GPA: 3.63</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">June 2011 - May 2015</span>
+            </div>
+          </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row">
+            <div class="resume-content mr-auto">
+              <h3 class="mb-0">Pragathi High School</h3>
+              <div class="subheading mb-3">SSC</div>
+              <p>GPA: 3.8</p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary">June 1999 - May 2009</span>
+            </div>
+          </div>
+
+        </div>
+      </section>
+        
+    <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="Gallery">
+          <h2 class="mb-5">Image Gallery</h2>
+         <div class="container">
+
+        <div class="row" style="width:100%">
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb rotate">
+                <a class="thumbnail">
+                    <img class="img-responsive"  src="Homeworks/Homework1/img/1.jpg" alt="">
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb shrink">
+                <a class="thumbnail">
+                    <img class="img-responsive"  src="Homeworks/Homework1/img/2.jpg" alt="">
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb grow">
+                <a class="thumbnail">
+                    <img class="img-responsive"  src="Homeworks/Homework1/img/3.JPG" alt="">
+                </a> 
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb rotate1">
+                <a class="thumbnail">
+                    <img class="img-responsive"  src="Homeworks/Homework1/img/4.JPG" alt="">
+                </a>
+            </div> 
+            
+            <br/><br/><br/><br/><br/><br/><br/>
+            
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb rotate">
+                <a class="thumbnail">
+                    <img class="img-responsive"  src="Homeworks/Homework1/img/5.jpg" alt="">
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb shrink">
+                <a class="thumbnail">
+                    <img class="img-responsive"  src="Homeworks/Homework1/img/6.jpg" alt="">
+                </a> 
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb grow">
+                <a class="thumbnail">
+                    <img class="img-responsive"  src="Homeworks/Homework1/img/7.jpg" alt="">
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb rotate1">
+                <a class="thumbnail">
+                    <img class="img-responsive"  src="Homeworks/Homework1/img/8.jpg" alt="">
+                </a>
+            </div>
+                        <br/><br/><br/><br/><br/><br/><br/>
+
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb rotate">
+                <a class="thumbnail">
+                    <img class="img-responsive"  src="Homeworks/Homework1/img/9.jpg" alt="">
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb shrink">
+                <a class="thumbnail">
+                    <img class="img-responsive"  src="Homeworks/Homework1/img/10.jpg" alt="">
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb grow">
+                <a class="thumbnail">
+                    <img class="img-responsive"  src="Homeworks/Homework1/img/11.jpg" alt="">
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb rotate1">
+                <a class="thumbnail">
+                    <img class="img-responsive"  src="Homeworks/Homework1/img/12.jpg" alt="">
+                </a>
+            </div>
+        </div>
+        </div>
+      </section>
+             
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="Homeworks">
+<!--         <div class="my-auto"> -->
+          <h2 class="mb-5">Homeworks</h2>
+          
+          
+          <div class="dropdown">
+            <button id="d" class="dropbtn">List Of Homeworks</button>
+                <div class="dropdown-content">
+				    <a href="#">Homework 1</a>
+				    <a href=" ">Homework 2</a>
+				    <a href="#">Homework 3</a>
+				    <a href="#">Homework 4</a>
+				    <a href="#">Homework 5</a>
+				    <a href="#">Homework 6</a>
+				    <a href="#">Homework 7</a>
+				    <a href="#">Homework 8</a>
+				    <a href="#">Homework 9</a>
+                </div>
+        
+          </div>
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="Exercises">
+          <h2 class="mb-5">In-Class Exercises</h2>
+                    
+          <div class="dropdown">
+            <button style= "background-color:#BD5D38; width:100%" class="dropbtn">List Of In-Class Exercises</button>
+                <div class="dropdown-content">
+				    <a href="#">Exercise 1</a>
+				    <a href=" ">Exercise 2</a>
+				    <a href="#">Exercise 3</a>
+              </div>
+          </div>
+
+
+      </section>
+
+
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="Projects">
+          <h2 class="mb-5">Projects</h2>
+
+          <div class="row" style="justify-content: center">
+          <div class="dropdown" style="margin-right:3%">
+              <button id="d1"  class="dropbtn">Individual Projects</button>
+              <div class="dropdown-content">
+                <a href="#">Project 1</a>
+                <a href="#">Project 2</a>
+                <a href="#">Project 3</a>
+              </div>
+            </div>
+
+            <div class="dropdown">
+              <button id="d2" class="dropbtn">Group Projects</button>
+              <div class="dropdown-content">
+                <a href="#">Project 1</a>
+                <a href="#">Project 2</a>
+                <a href="#">Project 3</a>
+              </div>
+            </div>
+              </div>
+      </section>
+
+      
+    <section class="p-3 p-lg-5 d-flex flex-column" id="validator">
+        <h3 class="mb-5">Code Validators</h3>
+        <footer>
+            <a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Fpeople.rit.edu%2Ffs2329%2F646%2Findex.php">
+                <img style="margin-left: 40%; bomargin-left: 40%; rder:0; width:88px; height:71px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2000px-HTML5_logo_and_wordmark.svg.png" alt="Valid CSS!" title="Valid CSS"/>
+        </a>
+        &nbsp;&nbsp;
+        <a href="https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fpeople.rit.edu%2Ffs2329%2F646%2FHomeworks%2FHomework1%2Fstyle.css&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en"> 
+            <img style="border:0; width:88px; height:31px" src="https://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS!" title="Valid CSS"/>
+        </a>
+        <br/>
+     
+    <p style="text-align: right">
+        <?php echo "Last modified: " . date ("F d Y H:i:s.", getlastmod()); 
+        ?>
+        </p>
+    
+    </footer> <!--end of footer -->
+
+      </section>
+        
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="Homeworks/Homework1/vendor/jquery/jquery.min.js"></script>
+    <script src="Homeworks/Homework1/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="Homeworks/Homework1/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="Homeworks/Homework1/js/resume.min.js"></script>
+
+  </body>
+
+</html>
